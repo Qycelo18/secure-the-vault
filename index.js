91880f08d26997ae344d6b65d46662e5c0c22008
Qyce Broussard <qbroussard18@gmail.com>
2026-03-18 21:04:56 -0500

    completed the steps of the README and wrote javascript to set the codes for the vault's combination lock.

---
+++ index.js
@@ -0,0 +1,13 @@
+let code1;
+code1 = 3 + 7;
+
+let code2 = 120 / 2 - 20;
+
+let code3 = (3 % 3) + 39;
+
+let message = "The vault has been secured. The combonation is:";
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message, codeA, codeB);
